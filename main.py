fabc9f6d08ede89419bd354142e733e51d25103c
Kevin Maurizi <kevin.maurizi@bzz.ch>
2024-09-30 09:05:57 +0200

    Ran pyink on branch solution

--- main.py
+++ main.py
@@ -2,25 +2,30 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET'])
 def home():
     """Gibt den Text 'Willkommen bei meiner Flask-App!' zurück."""
     return 'Willkommen bei meiner Flask-App!'
+
 
 @app.route('/info', methods=['GET'])
 def info():
     """Gibt den Text 'Dies ist die Info-Seite.' zurück."""
     return 'Dies ist die Info-Seite.'
 
+
 @app.route('/user/<username>', methods=['GET'])
 def user(username):
     """Gibt den Text 'Hallo, [username]!' zurück, wobei [username] durch den in der URL angegebenen Benutzernamen ersetzt wird."""
     return f'Hallo, {username}!'
 
+
 @app.route('/post', methods=['POST'])
 def post_data():
     """Akzeptiert Daten und gibt den Text 'Daten erfolgreich erhalten!' zurück."""
     return 'Daten erfolgreich erhalten!'
+
 
 @app.route('/feedback', methods=['GET', 'POST'])
 def feedback():
@@ -30,10 +35,12 @@ def feedback():
     elif request.method == 'POST':
         return 'Danke für Ihr Feedback!'
 
+
 @app.route('/item/<int:item_id>', methods=['GET'])
 def item(item_id):
     """Gibt den Text 'Artikel-ID: [item_id]' zurück, wobei [item_id] durch die in der URL angegebene Artikel-ID ersetzt wird."""
     return f'Artikel-ID: {item_id}'
+
 
 if __name__ == '__main__':
     app.run()
